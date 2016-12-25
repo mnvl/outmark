a2e958348eedb64553cda606539a285ff4cd9583
Manvel Avetisian <avetisian@gmail.com>
2016-12-25 11:35:43 +0300

    Imported image library and starting experiments with data.

---
+++ src/data_experiment_1.cc
@@ -0,0 +1,11 @@
+
+#include <string>
+#include <image/image.hpp>
+
+static std::string const DATA_DIR = "/large/data/Abdomen/RawData/";
+static std::string const TRAIN_DIR = DATA_DIR + "Training/";
+static std::string const TEST_DIR = DATA_DIR + "Testing/";
+
+int main() {
+    
+}
