84f5294adc0799ca2a079d77b998ea09dd90c262
Manvel Avetisian <avetisian@gmail.com>
2017-02-21 14:05:20 +0300

    model 2

--- NN/train.py
+++ NN/train.py
@@ -16,19 +16,21 @@ ds = AbdomenDataset()
 fe = FeatureExtractor(ds, 5, 0)
 
 D = 16
-H = 256
-W = 256
+H = 128
+W = 128
 minibatch_size = 2
 num_classes = len(ds.get_label_class_names())
 
-cnn = CNN2({"D": D, "H":H, "W":W, "num_classes":num_classes, "minibatch_size":minibatch_size})
+cnn = CNN2({
+  "D": D, "H": H, "W": W, "num_classes": num_classes, "minibatch_size": minibatch_size,
+  "conv1_size": 200, "conv2_size": 200, "conv3_size": 200, "conv4_size": 200 })
 
 steps = 1000
 val_accuracy_history = np.zeros(shape = steps)
 for i in range(1000):
   (X, y) = fe.get_random_training_batch(minibatch_size, D, H, W)
   X = X.reshape(minibatch_size, D, H, W, 1)
-  accuracy = cnn.fit(X, y)
+  (loss, accuracy) = cnn.fit(X, y)
 
   (X_val, y_val) = fe.get_random_validation_batch(minibatch_size, D, H, W)
   X_val = X_val.reshape(minibatch_size, D, H, W, 1)
@@ -37,4 +39,5 @@ for i in range(1000):
 
   mean_val_accuracy = np.mean(val_accuracy_history[i-20:i])
 
-  logging.info("step %d: accuracy = %f, val_accuracy = %f, mean_val_accuracy = %f" % (i, accuracy, val_accuracy, mean_val_accuracy))
+  logging.info("step %d: accuracy = %f, loss = %f, val_accuracy = %f, mean_val_accuracy = %f" % (
+    i, accuracy, loss, val_accuracy, mean_val_accuracy))
