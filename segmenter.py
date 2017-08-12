5a73047e35e69e8837df4ba43afd66cb595174aa
Manvel Avetisian <avetisian@gmail.com>
2017-08-12 11:32:40 +0300

    A segmenter class.

---
+++ segmenter.py
@@ -0,0 +1,99 @@
+
+import logging
+import unittest
+import numpy as np
+
+def cut(get, put, z, y, x):
+    for k in range(put.shape[0]):
+        for j in range(put.shape[1]):
+            for i in range(put.shape[2]):
+                put[k, j, i] = get[min(z + k, get.shape[0] - 1),
+                                   min(y + j, get.shape[1] - 1),
+                                   min(x + i, get.shape[2] - 1)]
+
+def paste(get, put, z, y, x):
+    for k in range(get.shape[0]):
+        if z + k >= put.shape[0]: continue
+
+        for j in range(get.shape[1]):
+            if y + j >= put.shape[1]: continue
+
+            for i in range(get.shape[2]):
+                if x + i >= put.shape[2]: continue
+
+                put[z + k, y + j, x + i] = get[k, j, i]
+
+class Segmenter:
+
+    def __init__(self, predictor, input_depth, input_height, input_width, image):
+        self.predictor = predictor
+        self.input_depth = input_depth
+        self.input_height = input_height
+        self.input_width = input_width
+        self.image = image
+
+    def predict(self):
+        image_depth, image_height, image_width = self.image.shape
+
+        prediction = np.zeros(self.image.shape, dtype = np.uint8)
+
+        for z in range(0, image_depth, self.input_depth):
+            for y in range(0, image_height, self.input_height):
+                for x in range(0, image_width, self.input_width):
+                    chunk = np.zeros((self.input_depth, self.input_height, self.input_width))
+                    cut(self.image, chunk, z, y, x)
+
+                    chunk_prediction = self.predictor(chunk)
+
+                    paste(chunk_prediction, prediction, z, y, x)
+
+        return prediction
+
+class TestSegmenter(unittest.TestCase):
+
+    def basic_test(self,
+                   input_depth=1,
+                   input_height=4,
+                   input_width=3,
+                   image_depth=1,
+                   image_height=11,
+                   image_width=11):
+        def predictor(x):
+            d, h, w = x.shape
+            assert d == input_depth
+            assert h == input_height
+            assert w == input_width
+            return x % 10
+
+        image = np.random.randint(
+            0, 200, (image_depth, image_height, image_width))
+        label = image % 10
+
+        segmenter = Segmenter(
+            predictor, input_depth, input_height, input_width, image)
+        prediction = segmenter.predict()
+
+        assert np.all(prediction == label)
+
+    def test_2d(self):
+        self.basic_test()
+
+    def test_3d(self):
+        self.basic_test(input_depth=6)
+
+    def test_big_2d(self):
+        self.basic_test(input_height = 224,
+                        input_width = 192,
+                        image_height = 512,
+                        image_width = 512)
+
+    def test_big_3d(self):
+        self.basic_test(input_depth = 8,
+                        input_height = 224,
+                        input_width = 192,
+                        image_depth = 48,
+                        image_height = 512,
+                        image_width = 512)
+
+if __name__ == '__main__':
+    unittest.main()
--- util.py
+++ util.py
@@ -1,4 +1,3 @@
-#! /usr/bin/python3
 
 import numpy as np
 
@@ -15,8 +14,8 @@ def iou(a, b, num_classes):
 
     s = 0.0
     for c in range(1, num_classes):
-        i = np.sum(np.logical_and(a == c, b == c), dtype = np.float32)
-        u = np.sum(np.logical_or(a == c, b == c), dtype = np.float32)
+        i = np.sum(np.logical_and(a == c, b == c), dtype=np.float32)
+        u = np.sum(np.logical_or(a == c, b == c), dtype=np.float32)
         s += i / (u + 1.0)
 
     return s / (num_classes - 1.0)
