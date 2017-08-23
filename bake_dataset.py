* add moving average to plots * give a more correct name to some funs

bake_dataset.py
@@ -73,7 +73,7 @@ def process(ds, index):
     image, label = ds.get_image_and_label(index)
     logging.debug("Original dtypes: %s (%s .. %s), %s." %
                   (image.dtype, np.min(image), np.max(image), label.dtype))
-    logging.debug(util.crappyhist(image))
+    logging.debug(util.text_hist(image))
 
     assert image.shape == label.shape
 

preprocess.py
@@ -107,7 +107,7 @@ class FeatureExtractor:
 
         if FLAGS.verbose_feature_extractor:
             logging.info(str(np.unique(label)) +
-                         "\n" + util.crappyhist(image, bins=20))
+                         "\n" + util.text_hist(image, bins=20))
 
 
         return image, label
@@ -120,7 +120,7 @@ class FeatureExtractor:
 
         if FLAGS.verbose_feature_extractor:
             logging.info(str(np.unique(label)) +
-                         "\n" + util.crappyhist(image, bins=20))
+                         "\n" + util.text_hist(image, bins=20))
         return image, label
 
     def crop_image_smart(self, image, label):

train.py
@@ -8,6 +8,7 @@ import datetime
 import logging
 import random
 import pickle
+import json
 import numpy as np
 import tensorflow as tf
 import scipy.misc
@@ -25,7 +26,6 @@ gflags.DEFINE_integer("batch_size", 8, "")
 gflags.DEFINE_integer("image_depth", 16, "")
 gflags.DEFINE_integer("image_height", 160, "")
 gflags.DEFINE_integer("image_width", 160, "")
-gflags.DEFINE_integer("shards_per_item", 10, "")
 gflags.DEFINE_string("settings", "LiTS", "")
 gflags.DEFINE_string("output", "./output/", "")
 gflags.DEFINE_string("mode", "train", "{hyperopt, train}")
@@ -43,16 +43,19 @@ class Trainer:
 
         self.feature_extractor = FeatureExtractor(
             self.S.image_width, self.S.image_height, self.S.batch_size)
-        self.feature_extractor_process = self.feature_extractor
-        # self.feature_extractor_process = FeatureExtractorProcess(
-        #     self.S.image_width, self.S.image_height, self.S.batch_size)
 
         self.model = VNet(settings)
         self.model.add_layers()
         self.model.add_optimizer()
 
-        self.val_accuracy_history = []
-        self.val_iou_history = []
+        self.history = util.AttributeDict()
+        self.history.val_accuracy = []
+        self.history.val_iou = []
+        self.history.train_loss = []
+        self.history.train_accuracy = []
+        self.history.train_iou = []
+        self.history.val_accuracy_estimate = []
+        self.history.val_iou_estimate = []
 
         self.step = 0
         self.model.start()
@@ -61,32 +64,37 @@ class Trainer:
         self.model.read(filepath + "tf")
 
         if os.path.isfile(filepath + "vars"):
-            with open(filepath + "vars", "rb") as f:
-                data = pickle.load(f)
-                self.step = data.get("step") + 1
+            try:
+                with open(filepath + "vars", "rb") as f:
+                    data = pickle.load(f)
+            except:
+                with open(filepath + "vars", "rt") as f:
+                    data = json.load(f)
+
+            self.step = data.get("step") + 1
+
+            history = data.get("history", None)
+            if history is not None:
+                self.history = util.AttributeDict(history)
 
     def write_model(self, filepath):
         self.model.write(filepath + "tf")
 
-        with open(filepath + "vars", "wb") as f:
-            data = {
-                "step": self.step,
-            }
-            pickle.dump(data, f)
+        data = {
+            "step": self.step,
+            "history": self.history,
+        }
+
+        with open(filepath + "vars", "wt") as f:
+            json.dump(data, f)
 
     def train(self, num_steps):
         validate_every_steps = min(num_steps, FLAGS.validate_every_steps)
 
         start_time = time.time()
 
-        train_loss_history = []
-        train_accuracy_history = []
-        train_iou_history = []
-        val_estimate_accuracy_history = []
-        val_estimate_iou_history = []
-
         while self.step < num_steps:
-            (X, y) = self.feature_extractor_process.get_random_training_batch()
+            (X, y) = self.feature_extractor.get_random_training_batch()
             X = np.expand_dims(X, 1)
             y = np.expand_dims(y, 1)
 
@@ -100,36 +108,35 @@ class Trainer:
             if (self.step + 1) % validate_every_steps == 0:
                 (val_accuracy, val_iou) = self.validate_full()
 
-                self.val_accuracy_history.append(val_accuracy)
-                self.val_iou_history.append(val_iou)
+                self.history.val_accuracy.append(float(val_accuracy))
+                self.history.val_iou.append(float(val_iou))
 
                 logging.info("[step %6d/%6d, eta = %s] accuracy = %f, iou = %f, loss = %f, segmentation: val_accuracy = %f, val_iou = %f" %
                              (self.step, num_steps, eta, train_accuracy, train_iou, loss, val_accuracy, val_iou))
             elif (self.step + 1) % FLAGS.estimate_every_steps == 0:
                 (val_accuracy, val_iou) = self.validate_fast()
 
-                train_loss_history.append(loss)
-                train_accuracy_history.append(train_accuracy)
-                train_iou_history.append(train_iou)
-                val_estimate_accuracy_history.append(val_accuracy)
-                val_estimate_iou_history.append(val_iou)
+                self.history.train_loss.append(float(loss))
+                self.history.train_accuracy.append(float(train_accuracy))
+                self.history.train_iou.append(float(train_iou))
+                self.history.val_accuracy_estimate.append(float(val_accuracy))
+                self.history.val_iou_estimate.append(float(val_iou))
 
                 g2i = image_server.graphs_to_image
-                images = [g2i("loss",
-                              range(len(train_loss_history)),
-                              train_loss_history),
-                          g2i("accuracy",
-                              range(len(train_accuracy_history)),
-                              train_accuracy_history,
-                              range(len(val_estimate_accuracy_history)),
-                              val_estimate_accuracy_history,
-                              label = ("train", "val")),
-                          g2i("iou",
-                              range(len(train_iou_history)),
-                              train_iou_history,
-                              range(len(val_estimate_iou_history)),
-                              val_estimate_iou_history,
-                              label = ("train", "val"))]
+                images = [g2i(self.history.train_loss,
+                              title = "loss",),
+                          g2i(self.history.train_accuracy,
+                              self.history.val_accuracy_estimate,
+                              title = "accuracy"),
+                          g2i(self.history.train_iou,
+                              self.history.val_iou_estimate,
+                              title = "iou"),
+                          g2i(self.history.val_accuracy,
+                              title = "segment accuracy",
+                              moving_average = False),
+                          g2i(self.history.val_iou,
+                              title = "segment iou",
+                              moving_average = False)]
                 image_server.put_images("graph", images, keep_only_last = True)
 
                 logging.info("[step %6d/%6d, eta = %s] accuracy = %f, iou = %f, loss = %f, estimation: val_accuracy = %f, val_iou = %f" %
@@ -141,7 +148,7 @@ class Trainer:
             self.step += 1
 
     def validate_fast(self):
-        X_val, y_val = self.feature_extractor_process.get_random_validation_batch()
+        X_val, y_val = self.feature_extractor.get_random_validation_batch()
         X_val = np.expand_dims(X_val, 1)
         y_val = np.expand_dims(y_val, 1)
 

util.py
@@ -2,6 +2,21 @@
 import numpy as np
 import logging
 
+
+class AttributeDict(dict):
+
+    def __getattr__(self, key):
+        if key not in self:
+            raise AttributeError(key)
+        return self[key]
+
+    def __setattr__(self, key, value):
+        self[key] = value
+
+        def __delattr__(self, key):
+            del self[key]
+
+
 def accuracy(a, b):
     return np.mean((a.flatten() == b.flatten()).astype(np.float32))
 
@@ -21,17 +36,24 @@ def iou(a, b, num_classes):
     return s / (num_classes - 1.0)
 
 
-def crappyhist(a, bins=40):
+def text_hist(a, bins=40):
     h, b = np.histogram(a, bins)
-    text = []
-
+    scale = 80.0 / np.amax(h)
+    text = ""
     for i in range(0, bins - 1):
-        text.append("%8.2f | %10d | %s" %
-                    (b[i], h[i - 1], '*' * int(70 * h[i - 1] / np.amax(h))))
+        text += ("%8.2f | %10d | %s\n" %
+                 (b[i], h[i - 1], '*' * int(scale * h[i - 1])))
+    text += "%8.2f" % b[bins]
+    return text
 
-    text.append("%8.2f" % (b[bins]))
 
-    return "\n".join(text)
+# window_size should be odd
+def moving_average(a, window_size=9):
+    a = np.concatenate(
+        (np.repeat(a[0], window_size // 2), a, np.repeat(a[-1], window_size // 2)))
+    W = np.ones(window_size) / float(window_size)
+    return np.convolve(a, W, "valid")
+
 
 def setup_logging():
     logging.basicConfig(level=logging.DEBUG,