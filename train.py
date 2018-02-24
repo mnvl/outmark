5d5b8acb2cf390a408462a6e58fcfc43ba077a72
Manvel Avetisian <avetisian@gmail.com>
2018-02-24 14:17:38 +0300

    more channels for LCTSC

--- train.py
+++ train.py
@@ -283,6 +283,7 @@ def make_best_settings():
         s.l2_reg = 1.0e-6
     elif FLAGS.settings == "LCTSC":
         s.learning_rate = 0.0005
+        s.num_conv_channels = 128
         s.num_classes = 6
         s.class_weights = [1.0] + [5.0]*5
         s.keep_prob = 0.5
