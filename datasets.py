numerious fixes in normalization

datasets.py
@@ -428,15 +428,14 @@ class LCTSCDataSet(DataSet):
 
         # normalize the image
         background = combined_image[0, 0, 0]
-        assert np.allclose(background, np.min(combined_image.reshape(-1)))
-
         if abs(background + 2000.0) < 1.0:
             combined_image = combined_image / 2000.0
         else:
             combined_image = (combined_image - 1024.0) / 1024.0
 
-        logging.info("Background color is %f, histogram: %s." % (
-            background, util.text_hist(combined_image.reshape(-1))))
+        logging.info("Background color is %f, minimal color is %f, histogram: %s." % (
+            background, np.min(combined_image.reshape(-1)),
+            util.text_hist(combined_image.reshape(-1))))
 
         return combined_image, combined_label
 

extract_dataset.py
@@ -76,8 +76,9 @@ def process(arguments):
 
     assert image.shape == label.shape, str(
         image.shape) + " != " + str(label.shape)
+    assert image.dtype == np.float32, str(image.dtype)
 
-    image = image.astype(np.int16)
+    image = (image * 1024).astype(np.int16)
     label = label.astype(np.uint8)
 
     info = {

train.py
@@ -221,10 +221,10 @@ class Trainer:
             label = label[0,:,:]
             pred = pred[0,:,:]
 
+        # image should be mostly in range (-1, 1)
         fig = plt.figure(figsize=(4, 4))
         ax1 = fig.add_subplot(111)
-        flat_image = image.reshape(-1)
-        ax1.hist(flat_image, bins=20, range=(min(np.min(flat_image), 0), max(np.max(flat_image), 1)))
+        ax1.hist(image.reshape(-1), bins=20, range=(min(np.min(image), -1), max(np.max(image), 1)))
         image_hist = image_server.figure_to_image(fig)
 
         # image should be mostly in range (-1, 1), so it's most probably data preparation problem
@@ -236,7 +236,7 @@ class Trainer:
         label = label.astype(np.uint8) * (250 // self.feature_extractor.get_num_classes())
         mask = np.dstack((eq, label, pred))
 
-        image_server.put_images(text, (image, label, pred, eq, mask, image_hist))
+        image_server.put_images(text, (image, label, pred, image_hist))
 
         if not save_to_disk:
             return